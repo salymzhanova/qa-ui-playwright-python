3f6c16ca6f54a24310a79b658b2968aa77f64d47
salymzhanova <salymzhanova@gmail.com>
2025-11-20 16:15:37 -0600

    Refactor login page, add user profiles, improve test structure

--- e2e/tests/test_login.py
+++ e2e/tests/test_login.py
@@ -1,8 +1,14 @@
 import pytest
 from e2e.pages.login_page import LoginPage
 
+
+LOGIN_USERNAME = "standard_user"
+LOGIN_PASSWORD = "secret_sauce"
+BASE_URL = "https://www.saucedemo.com"
+INVENTORY_URL = "https://www.saucedemo.com/inventory.html"
+
 def test_login(page):
     login_page = LoginPage(page)
-    page.goto("https://www.saucedemo.com")
-    login_page.login("standard_user", "secret_sauce")
-    assert page.url.endswith("inventory.html")
+    page.goto(BASE_URL)
+    login_page.login(LOGIN_USERNAME, LOGIN_PASSWORD)
+    assert page.url == INVENTORY_URL
