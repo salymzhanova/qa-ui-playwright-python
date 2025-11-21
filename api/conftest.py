492a9397d242f168a8dbb3b2985cde37c4a9de65
salymzhanova <salymzhanova@gmail.com>
2025-11-21 16:32:25 -0600

    Remove API tests from UI repo to separate UI/E2E and API tests

--- api/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import os
-from api.helpers.api_client import APIClient
-
-@pytest.fixture(scope="session")
-def api_client():
-    base_url = os.getenv("API_BASE_URL", "https://reqres.in/api")
-    return APIClient(base_url)
