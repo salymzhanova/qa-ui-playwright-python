02788bc544dcff0735145730498ba33eede5181e
salymzhanova <salymzhanova@gmail.com>
2025-12-12 21:23:19 -0600

    Create locators.py to centralize page locators and update tests

---
+++ e2e/pages/locators.py
@@ -0,0 +1,10 @@
+""" 
+Centralized locators for e2e tests.
+Organized by page/component for easy maintenance. 
+"""
+
+class LoginPageLocators:
+    USERNAME_INPUT = "#user-name"
+    PASSWORD_INPUT = "#password"
+    LOGIN_BUTTON = "#login-button"
+    ERROR_MESSAGE = "[data-test='error']"
--- e2e/tests/test_login.py
+++ e2e/tests/test_login.py
@@ -1,7 +1,7 @@
 import pytest
 from e2e.pages.login_page import LoginPage
-from e2e.utils.users import STANDARD_USER
-from e2e.utils.users import LOCKED_OUT_USER
+from e2e.utils.users import STANDARD_USER, LOCKED_OUT_USER
+from e2e.pages.locators import LoginPageLocators
 
 BASE_URL = "https://www.saucedemo.com/"
 INVENTORY_URL = "https://www.saucedemo.com/inventory.html"
@@ -18,6 +18,6 @@ def test_locked_out_user(page):
     login_page = LoginPage(page)
     page.goto(BASE_URL)
     login_page.login(LOCKED_OUT_USER)
-    error = page.locator("[data-test='error']")
+    error = page.locator(LoginPageLocators.ERROR_MESSAGE)
     assert error.is_visible()
     assert "locked out" in error.inner_text().lower()
